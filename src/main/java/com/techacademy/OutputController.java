252de4447473df3da9eb45519e4498815f18f8ba
smoto <smoto@Ryo-Sakamoto-PC>
2023-03-25 22:48:44 +0900

    "Thymeleafの利用"の課題提出用（不要な記述を削除）

--- src/main/java/com/techacademy/OutputController.java
+++ src/main/java/com/techacademy/OutputController.java
@@ -2,9 +2,7 @@ package com.techacademy;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -12,13 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
         @PostMapping("/output")
         public String postOutput(@RequestParam(name = "val", required = false) String val, Model model) {
-            // フォームから送信されてきた値をModelに登録
-            model.addAttribute("val", val);
-            // output.htmlに画面遷移
-            return "output";
-        }
-        @PostMapping("/input")
-        public String previous(@RequestParam(name = "val", required = false) String val, Model model) {
             // フォームから送信されてきた値をModelに登録
             model.addAttribute("val", val);
             // output.htmlに画面遷移
