a99bb88d564d02b8e0324741ddbe303b6f7a54fa
smoto <smoto@Ryo-Sakamoto-PC>
2023-03-25 22:34:23 +0900

    ”Thymeleafの利用”の課題提出用

--- src/main/java/com/techacademy/OutputController.java
+++ src/main/java/com/techacademy/OutputController.java
@@ -2,7 +2,9 @@ package com.techacademy;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -15,5 +17,11 @@ import org.springframework.web.bind.annotation.RequestParam;
             // output.htmlに画面遷移
             return "output";
         }
-
+        @PostMapping("/input")
+        public String previous(@RequestParam(name = "val", required = false) String val, Model model) {
+            // フォームから送信されてきた値をModelに登録
+            model.addAttribute("val", val);
+            // output.htmlに画面遷移
+            return "output";
+        }
 }
